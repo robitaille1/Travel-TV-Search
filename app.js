a80dd2712794ca60bf67ccfe9b2c7cfe1c091289
Lucas Robitaille <lrjames237@gmail.com>
2019-08-23 12:10:21 -0400

    atto findAllEpisodes functionality

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 'use strict';
 
 const searchUrl = 'http://api.tvmaze.com/shows';
-const episodeUrl = 'http://api.tvmaze.com/episodes/';
-let currentPage = 0;
 
 
-while(currentPage <= 173){
-    findAllShows();
+function loopPages() {
+  let currentPage = 0;
+  while(currentPage <= 173){
+    findAllShows(currentPage);
     currentPage++;
+  }
 }
-
-
 
 function formatQueryParams(params) {
     const queryItems = Object.keys(params)
@@ -18,14 +17,12 @@ function formatQueryParams(params) {
     return queryItems.join('&');
   }
 
-function findAllShows() {
+function findAllShows(currentPage) {
     const params = {
       page: currentPage,
     };
     const queryString = formatQueryParams(params)
     const url = searchUrl + '?' + queryString;
-  
-    // console.log(url);
 
     fetch(url)
     .then(response => {
@@ -36,22 +33,48 @@ function findAllShows() {
     })
     .then(responseJson => {
         for(var i = 0; i < responseJson.length; i++){
-            if(responseJson[i].genres[0] === "Food" && responseJson[i].genres[1] === "Travel"){
-                console.log(`${responseJson[i].name} - ${responseJson[i].id}`);
+            if(responseJson[i].genres[0] === "Travel" || responseJson[i].genres[1] === "Travel"){
+              let showList = [];
+              showList.push(`${responseJson[i].id}`);
+              findAllEpisodes(showList);
             }
         }
     })
     .catch(err => {
       $('#js-results').append(`There was an error. Please try again`);
     });
+
 }
 
+function findAllEpisodes(id) {
+  const episodeUrl = 'http://api.tvmaze.com/shows/'+ id + '/episodes'
+  fetch(episodeUrl)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(response.statusText);
+  })
+  .then(responseJson => {
+    for(var i = 0; i < responseJson.length; i++){
+      let episodeTitles = []
+      episodeTitles.push(responseJson[i].name);
+      displayResults(episodeTitles);
+      }
+    })
+  }
+
+function displayResults(titles) {
+    $('.js-display-list').append(`
+    <li>${titles}</li>
+  `)
+}
 
 function watchForm() {
     $('form').submit(event => {
       event.preventDefault();
       const querySearch = $('#js-search-query').val();
-      findAllShows();
+      loopPages();
     });
   }
 
