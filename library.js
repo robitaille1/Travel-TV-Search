7a4a1165ad4dea47f12e1cfc9a69bd14b7004b4d
Lucas Robitaille <lrjames237@gmail.com>
2019-08-30 14:48:39 -0500

    added show library

--- library.js
+++ library.js
@@ -1,86 +1,202 @@
 const STORE = [
     {
-        id: 
-        title:
-    }
+        id: 255,
+        title: "Anthony Bourdain: Parts Unknown"
+    },
     {
-        id: 
-        title:
-    }
+        id: 535,
+        title: "Anthony Bourdain: No Reservations"
+    },
     {
-        id: 
-        title:
-    }
+        id: 1201,
+        title: "Bizarre Foods with Andrew Zimmern"
+    },
     {
-        id: 
-        title:
-    }
+        id: 31919,
+        title: "The Zimmern List"
+    },
     {
-        id: 
-        title:
-    }
+        id: 32124,
+        title: "Andrew Zimmern's Bizarre World"
+    },
     {
-        id: 
-        title:
-    }
+        id: 604,
+        title: "Booze Traveler"
+    },
     {
-        id: 
-        title:
-    }
+        id: 15285,
+        title: "House Hunters International"
+    },
     {
-        id: 
-        title:
-    }
+        id: 709,
+        title: "An Idiot Abroad"
+    },
     {
-        id: 
-        title:
-    }
+        id: 22692,
+        title: "Travel Guides"
+    },
     {
-        id: 
-        title:
-    }
+        id: 23533,
+        title: "Walking the Americas"
+    },
     {
-        id: 
-        title:
-    }
+        id: 24016,
+        title: "World's Greatest Food Markets"
+    },
     {
-        id: 
-        title:
-    }
+        id: 24149,
+        title: "Around the World with Manu Feildel"
+    },
     {
-        id: 
-        title:
-    }
+        id: 4732,
+        title: "Beachfront Bargain Hunt"
+    },
     {
-        id: 
-        title:
-    }
+        id: 2837,
+        title: "BBQ Crawl"
+    },
     {
-        id: 
-        title:
-    }
+        id: 2902,
+        title: "Taco Trip"
+    },
     {
-        id: 
-        title:
-    }
+        id: 11441,
+        title: "Tropic of Cancer"
+    },
     {
-        id: 
-        title:
-    }
+        id: 12245,
+        title: "Great American Railroad Journeys"
+    },
     {
-        id: 
-        title:
-    }
+        id: 19666,
+        title: "Hidden America with Jonah Ray"
+    },
     {
-        id: 
-        title:
-    }
+        id: 11519,
+        title: "Could I Live There?"
+    },
     {
-        id: 
-        title:
-    }
+        id: 18766,
+        title: "The Nomad Chef"
+    },
     {
-        id: 
-        title:
-    }
+        id: 18029,
+        title: "A Cook Abroad"
+    },
+    {
+        id: 18030,
+        title: "A Cook's Tour"
+    },
+    {
+        id: 25601,
+        title: "Slow Train Through Africa with Griff Rhys Jones"
+    },
+    {
+        id: 24985,
+        title: "Rick Stein's Far Eastern Odyssey"
+    },
+    {
+        id: 24986,
+        title: "Rick Stein's Mediterranean Escapes"
+    },
+    {
+        id: 7979,
+        title: "A South American Journey with Jonathan Dimbleby"
+    },
+    {
+        id: 5795,
+        title: "Man v. Food"
+    },
+    {
+        id: 10814,
+        title: "Luke Nguyen's United Kingdom"
+    },
+    {
+        id: 11194,
+        title: "Follow Donal... to Europe"
+    },
+    {
+        id: 26457,
+        title: "Andy and Ben Eat Australia"
+    },
+    {
+        id: 26584,
+        title: "1,000 Places to See Before You Die"
+    },
+    {
+        id: 26924,
+        title: "Indian Ocean with Simon Reeve"
+    },
+    {
+        id: 28526,
+        title: "Luke Nguyen's Vietnam"
+    },
+    {
+        id: 28644,
+        title: "Paul Hollywood's Big Continental Road Trip"
+    },
+    {
+        id: 29795,
+        title: "Spice Trip"
+    },
+    {
+        id: 31202,
+        title: "Greatest Cities of the World with Griff Rhys Jones"
+    },
+    {
+        id: 31346,
+        title: "World's Busiest Cities"
+    },
+    {
+        id: 33743,
+        title: "Man v. Food"
+    },
+    {
+        id: 34046,
+        title: "Donal's Asian Baking Adventure"
+    },
+    {
+        id: 34213,
+        title: "Somebody Feed Phil"
+    },
+    {
+        id: 35142,
+        title: "Passport to Europe"
+    },
+    {
+        id: 35733,
+        title: "Luke Nguyen's Food Trail"
+    },
+    {
+        id: 36933,
+        title: "Africa: A Journey into Music"
+    },
+    {
+        id: 37687,
+        title: "No Passport Required"
+    },
+    {
+        id: 38121,
+        title: "Travels in Europe with Rick Steves"
+    },
+    {
+        id: 39238,
+        title: "BBQ with Bobby Flay"
+    },
+    {
+        id: 41021,
+        title: "Around the World by Train with Tony Robinson"
+    },
+    {
+        id: 41803,
+        title: "Street Food"
+    },
+    {
+        id: 3334,
+        title: "Travel Man: 48 Hours in..."
+    },
+    {
+        id: 3343,
+        title: "Globe Trekker"
+    },
 ]
