a185b0acf416b17938d4cf584481c0374e9bde31
Lucas Robitaille <lrjames237@gmail.com>
2019-08-30 16:32:23 -0500

    added google map functionality

--- app.js
+++ app.js
@@ -2,6 +2,35 @@
 
 let searchInput = '';
 
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: {lat: 37.0902, lng: 95.7129}
+  });
+  var geocoder = new google.maps.Geocoder();
+
+  $('form').submit(event => {
+    event.preventDefault();
+    geocodeAddress(geocoder, map);
+  });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+  var address = searchInput;
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      $('#map').removeClass('hidden');
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
 function loopStore() {
   for(let i = 0; i < STORE.length; i++){
     findAllEpisodes(STORE[i]);
